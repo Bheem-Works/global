b7131e6ac58a75c6a8b3421115195302b9c7477b
bheem-works <shokokomichan098@gmail.com>
2025-07-14 17:28:59 +0545

    arguments functions in the c++

--- Function.c
+++ Function.c
@@ -1,8 +1,23 @@
+// function with the arguments; 
 #include <stdio.h>
-int boo();
+int triva(int a, int b);
+
+// The wrapTriva functions wrap the triva functions & take the arguments and print the result.
+int wrapTriva();
+
 int main(){
-    boo(){
-        printf("hello mom");
-    }
-    return boo();
+    int userInput;
+    printf("Enter a number: ");
+    scanf("%d", &userInput);
+    printf("You have entered: %d\n", userInput);
+    wrapTriva();
+    return 0;
+}
+int wrapTriva(){
+    int result = triva(5, 10); // calling the function with arguments
+    printf("The result of triva is: %d\n", result);
+    return 0;
+}
+int triva(int a, int b){
+    return a + b; // this function returns the sum of two integers
 }
