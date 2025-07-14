d71cd14116b702ba6a22e0b96bda1bf25efae2b6
bheem-works <shokokomichan098@gmail.com>
2025-07-14 17:38:54 +0545

    function with the arguments in the c++

---
+++ function.cpp
@@ -0,0 +1,19 @@
+    #include <iostream>
+    int foo(int a, int b);
+    int boo();
+    int main(){
+        std::cout << "This is a functions values through the arguements \n";
+        foo(5, 10); // calling the function with arguments
+        return 0; 
+    }
+    int boo(){
+        int a = 5, b = 10;
+        foo(a, b); // calling the function with arguments
+        return 0;
+    }
+
+    int foo(int x, int y){
+        int sum = x + y; // this function returns the sum of two integers
+        std::cout << "The result of foo is: " << sum << "\n";
+        return sum; // returning the sum
+    }
