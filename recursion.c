03c30c4bff1f1c498f3db2f3cd528245dc387d25
bheem-works <shokokomichan098@gmail.com>
2025-07-17 23:43:24 +0545

    factorial in the c

--- recursion.c
+++ recursion.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+// wap for find out the factorial numbers; 
 // Recursion function 
 int foo(int i);
+ int fac(int j);
 int main() {
     // Your code here
-    int i = 3;
-    foo(i);
+    int userInput; 
+    printf("enter te positive numbers");
+    scanf("%d",&userInput);
+    printf("Factorial of the percent %d =%ld",userInput,fac(userInput));
     return 0;
 }
 
@@ -16,4 +20,13 @@ int foo(int i){
         foo(i - 1);
     }
 
+}
+
+ int fac(int j){
+    if(j >= 1){
+        return j * fac(j-1);
+        printf("execute tah hu nah vai");
+    }else {
+        return 1;
+    }
 }
