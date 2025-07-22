5aaa79ec849b804745bcbd9eef1bc6011d2518ed
bheem-works <shokokomichan098@gmail.com>
2025-07-23 00:08:16 +0545

    my logic

--- assignment/DisplayMark.c
+++ assignment/DisplayMark.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+
 // write a program to print the display value of the student name,id,age,class,mark; 
+
 struct DisplayStudent
+// ^^-----> (short for structure) is a way to group the different type of data together at the one name.
 {
     /* data */
     char name[30];
@@ -12,15 +15,22 @@ struct DisplayStudent
 
 int main() {
     // Declare a varaible of the type struct students; 
-    struct DisplayStudent TrashKoshi; //mero marji ma j sukai variable rakhum 
-    
-    // Assing the value to the members; 
-    strcpy(TrashKoshi.name,"koshiTrash");
+    struct DisplayStudent TrashKoshi;// ---> representing varaibles name;  
+/*    ||       ^^^ -------------^^^^ ? 
+      ||                |-> DisplayStudents is a like a blue print for the actual data and TrashKoshi is the varaibles names that holds the actuall data and represent it. 
+//    ||-> This is the keywords in the c that tells the compiler that you are working with the structure;       
+*/
+strcpy(TrashKoshi.name,"koshiTrash");
+//^^^---------->  strcpy is a function is function of string.h which allows the 'string' to be copied from the one place to another; 
+//          |-> it's takes the two arguments that are = (destination,source);
+//                                                          ||         ^^^---> the string you want to copy
+//                                                          ||-> where you want to copy the string;
     TrashKoshi.id = 589;
     TrashKoshi.mark = 65.7;
 
     // Printing;
-    printf("Your name is : %s\n",TrashKoshi.name);
-    printf("Your id is : %d\n",TrashKoshi.id);
+    printf("Your name is : %s\n",TrashKoshi.name);// Your name is: KoshiTrash;
+    printf("Your id is : %d\n",TrashKoshi.id);// Your id is : 589;
+
     return 0; 
 }
