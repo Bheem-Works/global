01dcbbb5c2f5d831cbf97941894eadaf59ad9327
bheem-works <arigatooooo100@gmail.com>
2025-07-29 16:46:11 +0545

    First push

---
+++ assignment/AcDsOderList.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+// int list(); 
+int accending();
+
+int main(){
+    // list();
+    accending();
+}
+
+int accending(){
+    // This will be the accending oder 
+    printf("acessing oder :");
+    
+    return 0;
+}
+// int list(){
+//     // created a structure to store the different data types value; 
+//     struct {
+//         char name [10];
+//         int age; 
+//         int id;
+//     }student[3]; 
+
+//     // Loops for enter the input to the structure variables; 
+//     for(int i = 0; i < 3; i++){
+//         printf("Enter the asked value \n");
+//         // Name ---------; 
+//         printf("Your name:");
+//         scanf("%s &name \n"); 
+
+//         // age 
+//         printf("enter your age");
+//         scanf("%d &age\n"); 
+//     }
+// }
---
+++ assignment/tempCodeRunnerFile.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+// int list(); 
+
+int main(){
+    // list();
+    int a; 
+    printf("Enter the numbers = ");
+    scanf("your number is : %d",&a); 
+    printf("your number is %d",a); 
+    return 0;
+}
+
+// int list(){
+//     // created a structure to store the different data types value; 
+//     struct {
+//         char name [10];
+//         int age; 
+//         int id;
+//     }student[3]; 
+
+//     // Loops for enter the input to the structure variables; 
+//     for(int i = 0; i < 3; i++){
+//         printf("Enter the asked value \n");
+//         // Name ---------; 
+//         printf("Your name:");
+//         scanf("%s &name \n"); 
+
+//         // age 
+//         printf("enter your age");
+//         scanf("%d &age\n"); 
+//     }
+// }
