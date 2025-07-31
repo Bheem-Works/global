9ae895fc42b0f388153b80ce7838eff51b40f23f
bheem-works <shokokomichan098@gmail.com>
2025-07-31 11:55:38 +0545

    homeworks record

--- assignment/AcDsOderList.c
+++ assignment/AcDsOderList.c
@@ -10,35 +10,39 @@ int main(){
 int accending(){
     // creating the structure; 
     struct{
-        int name[30];
+        char name[30];
         int id;
         int rank;
     } student [3]; 
     // deatail about the structure; 
-    struct student; 
     strcpy(student[0].name, "vim"); 
     student[0].id = 90; 
     student[0].rank = 2;     
 
+    // seconds value; 
+    strcpy(student[1].name, "thupa");
+    student[1].id = 0; 
+    
     // for loops for the structure; 
-    for(int i = 0; i < 2; i++){
-        printf("studentRank %d",student[i].rank);
-    }
-    // This will be the accending oder 
-    int oder[4] = {3,2,1,0};
+    // for(int i = 0; i < 2; i++){
+    //     printf("studentRank %d",student[i].rank);
+    //     printf("studentId %d",student[i].rank);
+    // }
+
+    // explaining the accending oders;
     int storeOder;
     for(int i = 0; i < 4; i++){
         for(int j = i + 1; j < 4; j++){
-            if(oder[i] > oder[j]){
-                storeOder = oder[i]; // it will direcltly store to the store array; no use of it
-                oder[i] = oder[j]; // store to the i it may be used; 
-                oder[j] = storeOder; // store it to the j and it also may used; 
+            if(student[i] > student[j]){
+                storeOder = student[i]; 
+                student[i] = student[j];  
+                student[j] = storeOder; 
             }
         }
     }
     // print; 
     for(int i = 0; i < 4; i++){
-        printf("%d",oder[i]);
+        printf("%d",student[i]);
     }
     return 0;
 }
