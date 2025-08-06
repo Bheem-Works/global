dd07bf843500713957de1e3e95b6280b065b8d0a
bheem-works <shokokomichan098@gmail.com>
2025-08-06 16:22:48 +0545

    read and write the file and take the output from the file

--- assignment/fileHandling/nbeQuestion.c
+++ assignment/fileHandling/nbeQuestion.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
-int main() {
-    char name[10],post[10];
+int main()
+{
+    char name[10], post[30];
     float salaray;
 
-    // let's  create  the file; 
-    FILE *file;   
-    file = fopen("document.txt","w");
-    printf("Enter the name,post and salary = ");
-    scanf("%s,%s,%f",&name,&post,&salaray);
-    fprintf(file,"%s,%s,%f",name,post,salaray);
+    // let's  create  the file;
+    FILE *file;
+    file = fopen("document.txt", "w");
+    // Store a multiple data by using the loop;
+    printf("Enter the name, post and salary: ");
+    scanf("%s %s %f", name, post, &salaray);
+    fprintf(file, "%s %s %.2f\n", name, post, salaray);
     fclose(file);
+
+    // for record the file;
+    FILE *fp;
+    fp = fopen("document.txt", "r");
+    printf("Name\tPost\tSalary\n");
+
+    while (fscanf(fp, "%s %s %f", name, post, &salaray) == 3)
+    {
+        printf("%s\t%s\t%.2f\n", name, post, salaray);
+    }
     return 0;
 }
