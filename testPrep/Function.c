c12689b4dc763ceb0d1cf5773715d84634366c13
bheem-works <shokokomichan098@gmail.com>
2025-08-14 20:32:58 +0545

    comment explain

--- testPrep/Function.c
+++ testPrep/Function.c
@@ -2,20 +2,26 @@
 
 #include <stdio.h>
 int factorial(int n);
+//   ^---fn()--- ^^----> this is the arguments; 
 int main() {
     int n; 
     printf("write the factorial numbers = ");
     scanf("%d",&n);
-
+    // ^---> taking the input value by using the scanf; 
     factorial(n);
+    //        ^----> Passing the value; 
     return 0;
 }
 
 int factorial(int n){
-    int fact = 1;
+         //    ^^^^----> Passing the arguments; 
+    int fact = 1; // |-> Seting the default value to the 1;  
+
     for(int i = 1; i <= n ; i++){
-        fact *= i;
+        fact *= i; // Multiplying the index N of times; 
+        // For examples; n = 2; 2 * 1 , 2 * 2 like this.
     }
-    printf("%d",fact);
+    printf("%d",fact); // Output show = 
     return fact;
+    // returning the fact; 
 }
